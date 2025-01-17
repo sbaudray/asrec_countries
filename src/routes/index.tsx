e8ee85be52828d5f103f7ceabb72ae13794e0a26
sbaudray <22913636+sbaudray@users.noreply.github.com>
2025-01-17 09:56:39 +0100

    Show all country names

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,14 +1,49 @@
-import * as React from 'react'
-import { createFileRoute } from '@tanstack/react-router'
+import * as React from "react";
+import { createFileRoute } from "@tanstack/react-router";
 
-export const Route = createFileRoute('/')({
+interface Country {
+  flags: string;
+  name: { common: string };
+}
+
+async function fetchAllCountries(): Promise<Array<Country>> {
+  const res = await fetch(
+    "https://restcountries.com/v3.1/all?fields=flag,flags,name,translations,capital,region,languages,currencies,demonyms",
+  );
+
+  return res.json();
+}
+
+export const Route = createFileRoute("/")({
   component: HomeComponent,
-})
+  loader: () => fetchAllCountries(),
+});
 
 function HomeComponent() {
+  const data = Route.useLoaderData();
+  console.log(data);
   return (
     <div className="p-2">
-      <h3>Welcome Home!</h3>
+      <h3>ASREC Countries</h3>
+      <CountryList countries={data} />
     </div>
-  )
+  );
+}
+
+function CountryList({ countries }: { countries: Country[] }) {
+  return (
+    <section>
+      {countries.map((country) => {
+        return <CountryItem country={country} />;
+      })}
+    </section>
+  );
+}
+
+function CountryItem({ country }: { country: Country }) {
+  return (
+    <article>
+      <h1>{country.name.common}</h1>
+    </article>
+  );
 }
